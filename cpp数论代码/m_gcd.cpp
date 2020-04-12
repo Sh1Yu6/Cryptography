7db5b4b534fba52b91e167d54ee63dda2cf19636
Sh1Yu6 <wangyaoxianlv99@icloud.com>
2020-04-12 21:20:29 +0800

    a

--- cpp数论代码/m_gcd.cpp
+++ cpp数论代码/m_gcd.cpp
@@ -6,7 +6,7 @@
 #        Author: Sh1Yu6
 #   Description: ---
 #        Create: 2020-04-12 18:40:00
-# Last Modified: 2020-04-12 19:00:13
+# Last Modified: 2020-04-12 21:20:09
 #***********************************************/
 #include <iostream>
 #include <string>
@@ -42,6 +42,37 @@ int my_lcm(int a, int b)
     return (a*b)/my_gcd(a, b);
 }
 
+void ex_gcd(int a, int b, int& x, int& y)
+{
+    
+    x = 0;
+    int x1 = 0;
+    int x2 = 1;
+
+    y = 1;
+    int y1 = 1;
+    int y2 = 0;
+    
+    int r = a % b;
+    int q = a / b;
+    while(r)
+    {
+        x = x2 - q * x1;
+        y = y2 - q * y1;
+
+        x2 = x1;
+        y2 = y1;
+
+        x1 = x;
+        y1 = y;
+
+        a = b;
+        b = r;
+        r = a % b;
+        q = a / b;
+    }
+
+}
 int main(int argc, char *argv[])
 {
     std::cout << "Please enter a b (e.g. 3 5):\n";
@@ -49,5 +80,9 @@ int main(int argc, char *argv[])
     std::cin >> a >> b;
     std::cout << "最大公约数:" << my_gcd(a, b) << "\n";
     std::cout << "最小公倍数:" << my_lcm(a, b) << "\n";
+    int x, y;
+    ex_gcd(a, b, x, y);
+    std::cout << "x: "<<x << " y: " << y; 
+    std::cout << "\n";
     return 0;
 }
