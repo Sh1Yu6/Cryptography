525cad2067183fec43b280bb0281e7d61e2c1cb6
Sh1Yu6 <wangyaoxianlv99@icloud.com>
2020-04-12 18:18:24 +0800

    a

--- cpp数论代码/modular_exponentiation.cpp
+++ cpp数论代码/modular_exponentiation.cpp
@@ -6,7 +6,7 @@
 #        Author: Sh1Yu6
 #   Description: ---
 #        Create: 2020-04-12 17:08:30
-# Last Modified: 2020-04-12 17:22:28
+# Last Modified: 2020-04-12 18:17:17
 #***********************************************/
 #include <iostream>
 #include <string>
@@ -15,6 +15,7 @@
 #include <cmath>
 #include <stdexcept>
 #include <deque>
+
 //b的n次方模m
 //n为n次方的二进制数
 long long func(const long long& b, const std::deque<int>& n, const long long& m)
@@ -32,12 +33,31 @@ long long func(const long long& b, const std::deque<int>& n, const long long& m)
     return x;
 }
 
+std::deque<int> int_to_bin(long long n, int b)
+{                                       
+    if(b <= 1 || n < 0)
+       return {};         
+                      
+    std::deque<int> v;
+    while(n != 0)         
+    {                
+       v.push_front(n%b);
+       n = n / b;            
+    }                 
+    return v;
+}  
+
 int main(int argc, char *argv[])
 {
-    long long b = 5;
-    std::deque<int> n{1, 0, 1, 0, 0, 0, 0, 1, 0, 0};
-    long long m = 645;
-    auto result = func(b, n, m); 
+    std::cout << "please enter b n m:\n";
+    long long b;
+    long long m; 
+    long long n;
+    std::cin >> b >> n >> m; 
+
+    auto n_bin = int_to_bin(n, 2);
+
+    auto result = func(b, n_bin, m); 
 
     std::cout << result << "\n";
     return 0;
